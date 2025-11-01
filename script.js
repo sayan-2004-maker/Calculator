d02462dd2a0a3f5b84f6ac0a6e0b615969cc10e3
Sayan Das <150497767+sayan-2004-maker@users.noreply.github.com>
2025-11-01 12:43:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+
+function appendValue(val) {
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
